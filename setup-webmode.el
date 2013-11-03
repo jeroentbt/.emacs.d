e3281bf1a7dc3105f4baf876d6cdf6fab2f38730
jeroentbt <jeroen@tinktenk.be>
2013-11-03 21:28:06 +0100

    Hook flycheck in to web-mode

--- setup-webmode.el
+++ setup-webmode.el
@@ -6,6 +6,7 @@
 (add-to-list 'auto-mode-alist '("\\.txp\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
 
-(add-hook 'web-mode-hook  'emmet-mode)
+(add-hook 'web-mode-hook 'emmet-mode)
+(add-hook 'web-mode-hook 'flycheck-mode)
 
 (provide 'setup-webmode)
