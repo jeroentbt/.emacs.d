ba7c28be209543eea6bcb1b93dd48b0b52d0aa7f
jeroentbt <jeroen@tinktenk.be>
2014-01-10 22:51:42 +0100

    [persp] load lazy
    Don't load any persps in new frames

--- conf/setup-persp.el
+++ conf/setup-persp.el
@@ -3,5 +3,7 @@
 (persp-mode t)
 (setq wg-morph-on nil) ;; no animation when setting a perspective
 (setq persp-nil-name "none")
+(setq persp-auto-resume nil)
+(setq persp-set-last-persp-for-new-frames nil)
 
 (provide 'setup-persp)
