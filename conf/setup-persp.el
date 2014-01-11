02f925f64f98fba81eb772ba42e20536e87b4bb6
jeroentbt <jeroen@tinktenk.be>
2014-01-11 22:02:23 +0100

    [persp] Acctivate persp-mode after settings
    To avoid default customize variables to pop up in customize.el

--- conf/setup-persp.el
+++ conf/setup-persp.el
@@ -1,9 +1,9 @@
 (require 'persp-mode)
 
-(persp-mode t)
 (setq wg-morph-on nil) ;; no animation when setting a perspective
 (setq persp-nil-name "none")
 (setq persp-auto-resume nil)
 (setq persp-set-last-persp-for-new-frames nil)
+(persp-mode t)
 
 (provide 'setup-persp)
