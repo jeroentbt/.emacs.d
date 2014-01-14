7b45cba27af9a15734cf3e325c683989130861af
jeroentbt <jeroen@tinktenk.be>
2014-01-14 09:27:41 +0100

    Add info to guidekey config

--- conf/setup-guidekey.el
+++ conf/setup-guidekey.el
@@ -1,6 +1,13 @@
 ;; guide-key
 (require 'guide-key)
-(setq guide-key/guide-key-sequence '("C-c p" "C-c !" "C-x r" "C-x 4" "C-x v" "C-x 8" "C-x x"))
+(setq guide-key/guide-key-sequence '("C-c p" ;; projectile
+                                     "C-c !" ;; flycheck
+                                     "C-x r" ;; ... stuff
+                                     "C-x 4" ;; file other window
+                                     "C-x v" ;; generic version controll
+                                     "C-x 8" ;; special chars
+                                     "C-x x" ;; persp
+                                     ))
 (guide-key-mode 1)
 (setq guide-key/recursive-key-sequence-flag t)
 (setq guide-key/popup-window-position 'bottom)
