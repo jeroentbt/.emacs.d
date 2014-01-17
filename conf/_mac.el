dbfc0d48734d19e698d4d137dfa7ebf866ad0ec6
jeroentbt <jeroen@tinktenk.be>
2014-01-18 00:18:38 +0100

    [OSX] Setenv for gpg-agent socket

--- conf/_mac.el
+++ conf/_mac.el
@@ -6,6 +6,13 @@
 ;; C-h v exec-path
 (exec-path-from-shell-initialize)
 
+;; This is for mu4e to be able to get gpg going (sig verifying etc)
+;; I was unable to M-x getenv RET GPG_AGENT_INFO (even if I manually set it,
+;; as gpg-agent form gpgtools.org does not export this)
+;; M-x shell-copy-environment-variable was no help either
+;; This should be no problem in *nix's
+(setenv "GPG_AGENT_INFO" "~/.gnupg/S.gpg-agent")
+
 (setq mac-option-modifier 'super)
 (setq mac-command-modifier 'meta)
 (setq ns-function-modifier 'hyper)
