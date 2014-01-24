bbae83027bbbcba486d13fe24f73c7d30b7a2b15
jeroentbt <jeroen@tinktenk.be>
2014-01-24 01:06:23 +0100

    [erc] Make buffer width follow window config

--- conf/setup-znc-erc.el
+++ conf/setup-znc-erc.el
@@ -52,4 +52,15 @@
                                     (current-nick . "#ERC Mentions"))
       )
 
+(make-variable-buffer-local 'erc-fill-column)
+(add-hook 'window-configuration-change-hook
+	   '(lambda ()
+	      (save-excursion
+	        (walk-windows
+		 (lambda (w)
+		   (let ((buffer (window-buffer w)))
+		     (set-buffer buffer)
+		     (when (eq major-mode 'erc-mode)
+		       (setq erc-fill-column (- (window-width w) 2)))))))))
+
 (provide 'setup-znc-erc)
