d32819a8e40639015febdfe44f171c7327b4e736
jeroentbt <jeroen@tinktenk.be>
2014-07-29 02:21:19 +0200

    Retangle config if changed only

--- init.el
+++ init.el
@@ -9,8 +9,24 @@
 (require 'cask "~/.cask/cask.el")
 (cask-initialize)
 
-(require 'ob-tangle)
-;; (setq debug-on-error t)
-(org-babel-load-file
- (expand-file-name "emacs-init.org"
-                   user-emacs-directory))
+
+;; only retangle if the org file is newer than the tangled file
+(defvar init-source-org-file 
+  (expand-file-name "emacs-init.org" 
+                    user-emacs-directory)
+  "The file that our emacs initialization comes form")
+
+(defvar init-source-el-file 
+  (expand-file-name "emacs-init.el" 
+                    user-emacs-directory)
+  "The file that our emacs initialization is generated into")
+
+(if (file-exists-p init-source-org-file)
+  (if (and (file-exists-p init-source-el-file)
+           (time-less-p (nth 5 (file-attributes init-source-org-file)) 
+                        (nth 5 (file-attributes init-source-el-file))))
+      (load-file init-source-el-file)
+    (progn (require 'ob-tangle)
+           ;; (setq debug-on-error t)
+	   (org-babel-load-file init-source-org-file)))
+  (load-file init-source-el-file))
