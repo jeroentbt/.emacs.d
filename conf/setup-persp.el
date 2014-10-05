8c2e315d92af9ff6cb8de8701a5c239206d87bb4
jeroentbt <jeroen@tinktenk.be>
2014-10-05 15:56:54 +0200

    cleanup

--- conf/setup-persp.el
+++
@@ -1,9 +0,0 @@
-(require 'persp-mode)
-
-(setq wg-morph-on nil) ;; no animation when setting a perspective
-(setq persp-nil-name "none")
-(setq persp-auto-resume nil)
-(setq persp-set-last-persp-for-new-frames nil)
-(persp-mode t)
-
-(provide 'setup-persp)
