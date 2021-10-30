0066101282ac6e83474b2a18bd87af2e4175942a
Jeroen Tiebout <jeroen@mailbox.org>
2021-10-30 23:48:00 +0200

    whitespace

--- init.el
+++ init.el
@@ -18,19 +18,19 @@
   (package-install 'org-plus-contrib))
 
 ;; only retangle if the org file is newer than the tangled file
-(defvar init-source-org-file 
-  (expand-file-name "emacs-init.org" 
+(defvar init-source-org-file
+  (expand-file-name "emacs-init.org"
                     user-emacs-directory)
   "The file that our emacs initialization comes form")
 
-(defvar init-source-el-file 
-  (expand-file-name "emacs-init.el" 
+(defvar init-source-el-file
+  (expand-file-name "emacs-init.el"
                     user-emacs-directory)
   "The file that our emacs initialization is generated into")
 
 (if (file-exists-p init-source-org-file)
   (if (and (file-exists-p init-source-el-file)
-           (time-less-p (nth 5 (file-attributes init-source-org-file)) 
+           (time-less-p (nth 5 (file-attributes init-source-org-file))
                         (nth 5 (file-attributes init-source-el-file))))
       (load-file init-source-el-file)
     (progn (require 'ob-tangle)
